9333fab213af0a31a1e5d7eb05422c5db02c0878
Eilia Araghbidikashani <earaghbi@amazon.com>
2026-09-24 20:22:24 -0700

    feat: remote IDE access as a transport on the access strategies

--- test/helm/aws-oidc/netpol_helpers_test.go
+++ test/helm/aws-oidc/netpol_helpers_test.go
@@ -21,9 +21,8 @@ const (
 	helmTemplateCmd   = "template"
 	helmOutputDirFlag = "--output-dir"
 
-	oauthStrategyFile     = "access-strategy/oauth-access-strategy.yaml"
-	bearerStrategyFile    = "access-strategy/bearer-access-strategy.yaml"
-	websocketStrategyFile = "access-strategy/websocket-access-strategy.yaml"
+	oauthStrategyFile  = "access-strategy/oauth-access-strategy.yaml"
+	bearerStrategyFile = "access-strategy/bearer-access-strategy.yaml"
 
 	// roleLabelKey is the node label / taint key used in nodeSelector and
 	// toleration test fixtures.
